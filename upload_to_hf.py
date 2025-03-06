582e387c518fe62849a11ca4890649178cad2b2c
JoonyeolChoiSKKU <joonyeol99@g.skku.edu>
2025-03-06 15:12:15 +0900

    Huggingface : Large File Upload

---
+++ upload_to_hf.py
@@ -0,0 +1,21 @@
+from huggingface_hub import HfApi
+
+REPO_ID = "joonyeol99/LSeg_ViT-to-ONNX"
+api = HfApi()
+api.upload_file(
+    path_or_fileobj="lseg_image_encoder.onnx",
+    path_in_repo="lseg_image_encoder.onnx",
+    repo_id=REPO_ID
+)
+
+# api.upload_file(
+#     path_or_fileobj="lseg_image_encoder.trt",
+#     path_in_repo="lseg_image_encoder.trt",
+#     repo_id=REPO_ID
+# )
+
+api.upload_file(
+    path_or_fileobj="demo_e200.ckpt",
+    path_in_repo="demo_e200.ckpt",
+    repo_id=REPO_ID
+)
